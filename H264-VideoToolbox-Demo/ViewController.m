964cf90cb7e49a9c8a7b9c0ea8574ae784dd6cff
殷汶杰 <yinwenjie@yinwenjiedeMacBook-Pro.local>
2018-02-12 21:27:48 +0800

    Add table view to main viewController

--- H264-VideoToolbox-Demo/ViewController.m
+++ H264-VideoToolbox-Demo/ViewController.m
@@ -10,7 +10,9 @@
 
 #include "include/libavformat/avformat.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@property (nonatomic, strong) NSArray *tableViewTitles;
 
 @end
 
@@ -19,13 +21,45 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.tableViewTitles = @[@"Albumn", @"Bundle"];
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - UITableView datasource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 2;
+}
 
+- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"abc"];
+    if (nil == cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"abc"];
+        cell.textLabel.lineBreakMode = NSLineBreakByTruncatingMiddle;
+    }
+    cell.textLabel.text = self.tableViewTitles[indexPath.row];
+    
+    return cell;
+}
+
+#pragma mark - UITableView Delegate
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    switch (indexPath.row) {
+        case 0:
+            NSLog(@"Play Video from Albumn.");
+            break;
+            
+        case 1:
+            NSLog(@"Play Video from Bundle.");
+            break;
+        default:
+            break;
+    }
+}
 @end
